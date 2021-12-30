5664734ff6b0a658ed739510be195113b49a36af
melipass <32807254+melipass@users.noreply.github.com>
2021-12-30 11:26:15 -0300

    Cleans up code, final touches on several files

--- src/rovibpy/visualize.py
+++ src/rovibpy/visualize.py
@@ -1,43 +1,98 @@
 import numpy as np
 import pandas as pd
-import plotly
 import plotly.graph_objs as go
 
 class Visualize:
     """
-    Visualize the results of the simulation.
+    A class used to visualize the results of the simulation.
 
     Attributes
     ----------
-    data : numpy.ndarray
-        The data to be visualized.
+    wavefunctions: pd.DataFrame
+        The wavefunctions obtained after running the simulation.
+    energies: numpy.ndarray
+        The energies obtained after running the simulation.
+
+    Methods
+    -------
+    plot_data(rmin, rmax, step, Vr, r)
+        Plots the data given the same parameters that were used in the simulation.
     """
 
     def __init__(self, wavefunctions, energies):
-        """
-        Initialize the Visualize class.
+        """Initialize the Visualize class.
 
         Parameters
         ----------
-        data : numpy.ndarray
-            The data to be visualized.
+        wavefunctions: pd.DataFrame
+            The wavefunctions obtained after running the simulation.
+        energies: numpy.ndarray
+            The energies obtained after running the simulation.
         """
         self.wavefunctions = pd.DataFrame(data=wavefunctions)
         self.energies = energies
 
-    def plot_data(self, rmin, rmax, step, Vr):
-        """
-        Plot the data.
+    def plot_data(self, rmin, rmax, step, Vr, r):
+        """Plots the data using the default renderer engine for the current environment.
+        
+        Based on Plotly's choice of renderer, this function will plot the data as a cell output
+        in Jupyter notebooks or open a static HTML file in Python scripts, but won't save this HTML file.
+
+        Parameters
+        ----------
+        rmin: float
+            The minimum value of the radial coordinate.
+        rmax: float
+            The maximum value of the radial coordinate.
+        step: float
+            The step size of the radial coordinate.
+        Vr: numpy.ndarray
+            The potential energy curve.
+        r: numpy.ndarray
+            The radial coordinate.
         """
         X = np.linspace(rmin,rmax, int((rmax-rmin)/step+1))
         fig = go.Figure()
         #w = np.linspace((Vr/1000).min(), 0, 16)
-        for i in range(self.df.shape[1]-1):
-            fig.add_trace(go.Scatter(y=self.df.iloc[:,i]/step+self.energies[i]*219474.63067, x=X))
+        for i in range(self.wavefunctions.shape[1]-1):
+            fig.add_trace(go.Scatter(y=self.wavefunctions.iloc[:,i]/step+self.energies[i]*219474.63067, x=X))
         fig.add_trace(go.Scatter(x=[x for x in r if x < 50], y=Vr))
         fig.update_layout(
             autosize=False,
             width=1800,
-            height=800)
+            height=800,
+            yaxis_title="[cm⁻¹]",
+            xaxis_title="[Bohr]",
+        )
         fig.show()
-        plotly.offline.plot({}, auto_open=True)
+
+    def plot_in_browser(self, rmin, rmax, step, Vr, r):
+        """Plots the data as an interactive JavaScript object in a static HTML file that opens in the default browser.
+
+        Parameters
+        ----------
+        rmin: float
+            The minimum value of the radial coordinate.
+        rmax: float
+            The maximum value of the radial coordinate.
+        step: float
+            The step size of the radial coordinate.
+        Vr: numpy.ndarray
+            The potential energy curve.
+        r: numpy.ndarray
+            The radial coordinate.
+        """
+        X = np.linspace(rmin,rmax, int((rmax-rmin)/step+1))
+        fig = go.Figure()
+        #w = np.linspace((Vr/1000).min(), 0, 16)
+        for i in range(self.wavefunctions.shape[1]-1):
+            fig.add_trace(go.Scatter(y=self.wavefunctions.iloc[:,i]/step+self.energies[i]*219474.63067, x=X))
+        fig.add_trace(go.Scatter(x=[x for x in r if x < 50], y=Vr))
+        fig.update_layout(
+            autosize=False,
+            width=1800,
+            height=800,
+            yaxis_title="[cm⁻¹]",
+            xaxis_title="[Bohr]",
+        )
+        fig.show(renderer='browser')
